d60cc207f3e5317dc7edbcccdfa74cdcd62aa0fe
Varun Kedia <purplevarun@gmail.com>
2020-12-20 02:39:24 +0530

    1st part done

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-    <title>Varun</title>
+    <title>Varun Kedia</title>
     <!-- <link rel="stylesheet" href="styled.css"> -->
     <style>
         body{
@@ -36,6 +36,8 @@
   <body>
     <!-- <h1>Hello, world!</h1> -->
     <h1 style="text-align:center;padding:30px">test login page</h1>
+    <h4 style="text-align:right;">created on 19/12/2020</h4>
+    <h4 style="text-align:right;">database insertion complete</h4>
     <p style="text-align:center;padding:30px">
         <button type="button" class="btn btn-primary btn-lg" id="signin">Sign in</button>
     </p>
