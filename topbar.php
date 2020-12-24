05cf8d7442f7444abaa379890e97e1980301937e
Varun Kedia <purplevarun@gmail.com>
2020-12-25 03:41:04 +0530

    local storage

--- topbar.php
+++ topbar.php
@@ -6,20 +6,21 @@
     
 </div>
 <script>
-    document.body.style.backgroundColor="white";
-    document.body.style.color="black";
+    if(localStorage.getItem("mode")=="dark"){
+        document.body.style.backgroundColor="black";
+        document.body.style.color="white";
+    }
+    
     function dark(){    
-        if (document.body.style.color=="black") {
+        if (document.body.style.color=="black" && document.body.style.backgroundColor=="white") {
             document.body.style.color="white";
-        }
-        else{
-            document.body.style.color="black";
-        }
-        if(document.body.style.backgroundColor=="white"){
             document.body.style.backgroundColor="black";
+            localStorage.setItem("mode","dark");
         }
-        else{
+        if (document.body.style.color=="white" && document.body.style.backgroundColor=="black") {
+            document.body.style.color="black";
             document.body.style.backgroundColor="white";
+            localStorage.clear();
         }
         
     }
